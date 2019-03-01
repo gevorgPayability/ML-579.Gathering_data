c957d77de16418bbca792144de9d6eb598a49ce4
gevorgPayability <gevorg@payability.com>
2019-03-01 13:06:59 +0100

    improved functions

--- prepare_data_functions.py
+++ prepare_data_functions.py
@@ -7,16 +7,19 @@ from collections.abc import Iterable
 
 def get_postgress_data(sql, user, password, col_index=None):
     """get data from payability posgress. In develop"""
-    txt_ = 'postgresql://{}:{}@database.payability.com:5432/payability_v2'.format(
+    connection_text = 'postgresql://{}:{}@database.payability.com:5432/payability_v2'.format(
         user, password)
-    engine = sqlalchemy.create_engine(txt_)
+    engine = sqlalchemy.create_engine(connection_text)
     data = pd.read_sql_query(
         sql, engine, index_col=col_index, parse_dates=True)
+    # close connection
+    engine.dispose()
 
     return(data)
 
 
-def get_info_from_notifications(data, notification_colum, columns, date_column, period=90):
+def get_info_from_notifications(data, notification_colum, columns, date_column,
+                                period=90, return_errors=False):
     """Short summary.
     Function return new dataframe with information about notifications type
     seller received in selected period of time
@@ -37,6 +40,7 @@ def get_info_from_notifications(data, notification_colum, columns, date_column, 
         pandas dataframe.
 
     """
+    error_list = []
     df = data.copy()
     df[date_column] = pd.to_datetime(df[date_column])
 
@@ -68,8 +72,13 @@ def get_info_from_notifications(data, notification_colum, columns, date_column, 
                                 other_message.append(0)
                         if np.max(other_message) == 0:
                             df.loc[index, 'other'] += 1
-        except:
-            print('Something unexpected happened at index', index)
+        except Exception as e:
+            message = 'Something unexpected happened at index' + index + '. Error message: ' + e
+            error_list.append(message)
+            print(message)
+
+    if return_errors:
+        return df, error_list
 
     return(df)
 
@@ -114,7 +123,8 @@ def has_active_loan(row, loan_column, date_column):
                 return np.max(has_a_loan)
             else:
                 return 0
-        except:
+        except Exception as e:
+            print(e)
             return np.NaN
     else:
         return 0
@@ -141,7 +151,8 @@ def number_of_loans(row, loan_column):
         try:
             keys = list(record)
             return len(keys)
-        except:
+        except Exception as e:
+            print(e)
             return np.NaN
     else:
         return 0
@@ -203,59 +214,45 @@ def fill_missing_dates_by_supplier(data, by_column='mp_sup_key'):
 
 
 def interpolate_missing_values(data, cls_to_interpolate, by_column='mp_sup_key',
-                               limit=14, limit_area='inside'):
-    """[Function fills missing values lineary.]
-
+                               limit=60):
+    """[summary]
+    Function interpolates missing values lineary. For example when there
+    is sequence 2, NaN, 4 function fills NaN value with 3.
     Arguments:
-        data {[pandas dataframe]} -- [pandas dataframe with datetimeindex]
-        cls_to_interpolate {[list]} -- [columns to fill]
+        data pandas dataframe -- data to fill missing values
+        cls_to_interpolate list of strings -- list of column to interpolate
 
     Keyword Arguments:
         by_column {str} -- [column to group by] (default: {'mp_sup_key'})
-        limit {int} -- [description] (default: {14})
-        limit_area {str} -- [description] (default: {'inside'})
+        limit {int} -- [limit days of filling] (default: {60})
+
+    Returns:
+        [type] -- [description]
     """
 
     df = data.copy()
-    df.index.rename('date', inplace=True)
     cls_to_stay = list(set(df.columns) - set(cls_to_interpolate))
     cls_to_stay.append('date')
 
     suppliers = data[by_column].unique()
     print('Number of suppliers is', len(suppliers))
-    supplier_dataframes_list = []
 
-    for index, supplier in enumerate(suppliers):
-        if index % 200 == 0:
-            print(index)
-        df_supplier = df.loc[df[by_column] == supplier]
-        df_supplier = df_supplier[cls_to_interpolate].interpolate(
-            limit=limit, limit_area=limit_area)
-        df_supplier['mp_sup_key'] = supplier
-        supplier_dataframes_list.append(df_supplier)
+    df[cls_to_interpolate] = df.groupby(by_column)[cls_to_interpolate].transform(
+        lambda x: x.interpolate(limit=limit, limit_area='inside'))
 
-    new_df = pd.concat(supplier_dataframes_list)
-
-    new_df['mp_sup_key'] = new_df.mp_sup_key.astype(str)
-    df['mp_sup_key'] = df.mp_sup_key.astype(str)
-
-    df.reset_index(inplace=True)
-    new_df.reset_index(inplace=True)
-
-    final = pd.merge(df[cls_to_stay], new_df, on=['mp_sup_key', 'date'])
-
-    return(final)
+    return df
 
 
-def fill_missing_values_from_notification(data, columns_from_notifications, limit=7, by_column='mp_sup_key'):
-    """Function fill missing values in columns from notifications. It fill forwards
+def fill_missing_values_from_notification(data, columns_from_notifications,
+                                          limit=30, by_column='mp_sup_key'):
+    """ Function fill missing values in columns from notifications. It fill forwards
     and backwards by given (limit) day
 
     Arguments:
         data {[pandas dataframe]} -- [pandas dataframe with notification columns]
 
     Keyword Arguments:
-        limit {int} -- [limit day length for fill] (default: {7})
+        limit {int} -- [limit day length for fill] (default: {30})
         by_column {str} -- [group by column] (default: {'mp_sup_key'})
 
     Returns:
@@ -265,26 +262,10 @@ def fill_missing_values_from_notification(data, columns_from_notifications, limi
     df = data.copy()
     suppliers = df[by_column].unique()
     print('Number of suppliers is', len(suppliers))
-    supplier_dataframes_list = []
 
-    for index, supplier in enumerate(suppliers):
-        if index % 200 == 0:
-            print(index)
-        df_supplier = df.loc[df[by_column] == supplier]
-        df_supplier[columns_from_notifications].fillna(
-            'ffill', limit=limit, inplace=True)
-        df_supplier[columns_from_notifications].fillna(
-            'bfill', limit=limit, inplace=True)
-        supplier_dataframes_list.append(df_supplier)
-    return pd.concat(supplier_dataframes_list)
+    df[columns_from_notifications] = df.groupby(by_column)[columns_from_notifications].transform(
+        lambda x: x.fillna(method='ffill', limit=limit))
+    df[columns_from_notifications] = df.groupby(by_column)[columns_from_notifications].transform(
+        lambda x: x.fillna(method='bfill', limit=limit))
 
-
-def some_func(a, b):
-    """[summary]
-
-    Arguments:
-        a {[str]} -- [name of your mom]
-        b {[int]} -- [age of your mom]
-    """
-
-    print('Hello world', a)
+    return df
